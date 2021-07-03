3c44bee08dfc5978c2276f08597aacb00bb3965f
chiharu <48370600+chiharu-matsukura@users.noreply.github.com>
2021-07-03 21:22:34 +0900

    use useWatch

--- src/CustomTextField.tsx
+++ src/CustomTextField.tsx
@@ -1,10 +1,14 @@
 import { TextField } from "@material-ui/core";
 import React, { useEffect } from "react";
-import { useFormContext, Controller } from "react-hook-form";
+import { useFormContext, Controller, useWatch } from "react-hook-form";
 
 const CustomTextField: React.FC<{ name: string }> = ({ name }) => {
-  const { control, watch } = useFormContext();
-  const textBoxValue = React.useMemo(() => watch(name), [watch, name]);
+  const { control } = useFormContext();
+  const textBoxValue = useWatch({
+    control,
+    name, // without supply name will watch the entire form, or ['firstName', 'lastName'] to watch both
+    defaultValue: "default" // default value before the render
+  });
   useEffect(() => {
     console.log(textBoxValue);
   }, [textBoxValue]);
